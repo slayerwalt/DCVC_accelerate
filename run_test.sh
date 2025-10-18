a704ba6d9e933b238de913af0a3a7ca3fc02c849
slayerwalt <slayerwalt0603@outlook.com>
2025-10-18 22:21:13 +0800

    add 1080Ti result

--- run_test.sh
+++ run_test.sh
@@ -2,10 +2,10 @@ python test_video.py \
     --model_path_i ./checkpoints/cvpr2025_image.pth.tar \
     --model_path_p ./checkpoints/cvpr2025_video.pth.tar \
     --rate_num 4 \
-    --test_config ./dataset_config_onlyHEVCB.json \
+    --test_config ./configs/dataset_config_onlyHEVCB_Lab.json \
     --cuda 1 -w 1 --write_stream 1 \
     --force_zero_thres 0.12 \
-    --output_path output_HEVC.json \
+    --output_path ./results/output_HEVC_B_GTX1080Ti.json \
     --force_intra_period -1 \
     --reset_interval 64 \
     --force_frame_num -1 \
